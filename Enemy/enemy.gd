773d820e63dd84d536899320f44de1dd473d1116
Hone Rico Tamatea <30087565@student.toiohomai.ac.nz>
2025-09-19 11:54:41 +1200

    Set up recoil, weapon damage and enemy damage

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,25 +1,35 @@
-
 extends CharacterBody3D
+class_name Enemy
 
 
-const SPEED = 5.0
+const SPEED = 4.5
 const JUMP_VELOCITY = 4.5
-@export var attack_range = 1.5
+@export var attack_damage := 20
+@export var max_hitpoints: = 100
+@export var attack_range := 1.5
 @onready var enemy: CharacterBody3D = $"."
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 var player
-var provoked := false 
+var provoked := false
 var aggro_range := 12.0
+
+var hitpoints: int = max_hitpoints:
+	set(value):
+		hitpoints = value
+		if hitpoints <= 0:
+			queue_free()
+		provoked = true
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
 	
-	
-func _process(_delta: float) -> void:
+
+func _process(delta: float) -> void:
 	if provoked:
 		navigation_agent_3d.target_position = player.global_position
-	
+
+
 func _physics_process(delta: float) -> void:
 	var next_position = navigation_agent_3d.get_next_path_position()
 	
@@ -32,11 +42,11 @@ func _physics_process(delta: float) -> void:
 	
 	if distance <= aggro_range:
 		provoked = true
-	
+		
 	if provoked:
 		if distance <= attack_range:
 			animation_player.play("Attack")
-	
+		
 	if direction:
 		look_at_target(direction)
 		velocity.x = direction.x * SPEED
@@ -45,13 +55,13 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	move_and_slide() 
-
+	move_and_slide()
+	
 func look_at_target(direction: Vector3):
-	var adjusted_direction = direction 
+	var adjusted_direction = direction
 	adjusted_direction.y = 0
 	look_at(global_position + adjusted_direction, Vector3.UP, true)
-
-
+	
 func attack():
-	print("enemy Attack")
+	print("Enemy Attack")
+	player.hitpoints -= attack_damage
